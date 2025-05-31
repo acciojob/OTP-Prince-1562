920bb52d91d2c1fb0b1f843229f7f8b6b290c7e9
Prince-1562 <princemakani1562@gmail.com>
2025-05-31 08:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If required.// script.js
+const inputs = document.querySelectorAll('.code');
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && e.target.value.length === 0 && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
